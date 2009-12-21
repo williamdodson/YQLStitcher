f37223912e602f019a05b68d027ea5fd4c5200a8
obxdesignworks <info@obxdesignworks.com>
2009-12-21 18:25:40 -0500

    refactored class to use cURL

--- yqlstitcher.class.php
+++ yqlstitcher.class.php
@@ -98,7 +98,15 @@ class YQLStitcher {
     $destination = $this->cache_file_name($url);
     
     if((!file_exists($destination)) || (filemtime($destination) < ($_SERVER['REQUEST_TIME']-$this->TIMEOUT))) {
-    	$data = file_get_contents($url);
+    	/**
+    	 * @note Refactored to use cURL instead of file_get_contents()
+    	 * @date 2009-12-21:18:24:21
+    	 */
+    	$ch = curl_init(); 
+      curl_setopt($ch, CURLOPT_URL, $url); 
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+      $data = curl_exec($ch); 
+      curl_close($ch);
     	
       if ($data === false) {
         $this->errors(

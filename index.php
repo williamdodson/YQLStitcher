bdc09d32d9480ef371921d0848be721e7626c340
obxdesignworks <info@obxdesignworks.com>
2009-12-21 19:07:23 -0500

    Moved demo into the root folder

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ function get_file_dir() {
 }
 $cwd = get_file_dir();
 // require the YQLStitcher class
-require_once './YQLStitcher/yqlstitcher.class.php';
+require_once $cwd . 'yqlstitcher.class.php';
 // cache folder
-$cache = './cache';
+$cache = $cwd . '/cache';
 // time in seconds to retain the cache file (defaults to 1 hour)
 $timeout = 60;
 //feeds we want to stitch together
